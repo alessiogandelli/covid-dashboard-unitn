5a5a92a816d063d52c7079b96a134ba864ffcd6c
alessiogandelli <alessiogandelli99@gmail.com>
2022-06-15 22:39:23 +0200

    clean code

--- src/database.py
+++ src/database.py
@@ -3,6 +3,12 @@ import paho.mqtt.client as mqtt #import the client1
 import time
 import pandas as pd
 
+
+insert_data = ''' insert into cases values ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )'''
+insert_regions = ''' insert into regions values ( ?,?,?,?,?,?,?,? )'''
+insert_age = ''' insert into age values ( ?,?,?,?,? )'''
+
+
 class Database:
     def __init__(self, name):
         self._conn = sqlite3.connect(name)
@@ -47,41 +53,10 @@ class Database:
     def from_pandas(self, df, table_name, if_exists='replace'):
         df.to_sql(table_name, self.connection, if_exists=if_exists, index=False)
 
-
-
-
-broker_address="broker.hivemq.com"
-#broker_address="iot.eclipse.org"
-print("creating new instance")
-client = mqtt.Client("pyclient") #create new instance
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-
-
-print("Subscribing to topic","covid_italy")
-client.subscribe("covid_italy")
-
-print("Subscribing to topic","covid_italy_col")
-client.subscribe("covid_italy_col")
-
-print("Subscribing to topic","covid_italy_age")
-client.subscribe("covid_italy_age")
-
-print("Subscribing to topic","covid_italy_region")
-client.subscribe("covid_italy_region")
-
-db = Database('covid.sqlite')
-
-insert_data = ''' insert into cases values ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )'''
-insert_regions = ''' insert into regions values ( ?,?,?,?,?,?,?,? )'''
-insert_age = ''' insert into age values ( ?,?,?,?,? )'''
-
-
 def on_message(client, userdata, message):
     data = eval(str(message.payload.decode("utf-8")))
 
     if message.topic == "covid_italy_col":
-
         df = pd.DataFrame(columns=data['cols'])
         db.from_pandas(df, data['table'])
         print('table', data['table'], 'created')
@@ -98,10 +73,33 @@ def on_message(client, userdata, message):
         db.execute(insert_regions, tuple(data))
         db.commit()
 
-client.on_message=on_message        #attach function to callback
+# set up the mqtt client
+broker_address="broker.hivemq.com"
+client = mqtt.Client("pyclient")   #create new instance
+client.connect(broker_address)     #connect to broker
+client.on_message=on_message       #attach function to callback
+print("Client connected")
+
+# subscribe to topics
+print("Subscribing to topic","covid_italy_col")
+print("Subscribing to topic","covid_italy_age")
+print("Subscribing to topic","covid_italy_region")
+print("Subscribing to topic","covid_italy")
+
+client.subscribe("covid_italy")
+client.subscribe("covid_italy_col")
+client.subscribe("covid_italy_age")
+client.subscribe("covid_italy_region")
+
+
+db = Database('covid.sqlite')
 
 client.loop_forever()
 
-#db.from_pandas(data, 'covid_italy')
+
+
+
+
+
 
 
---
+++ src/init.py
@@ -0,0 +1,60 @@
+#%%
+import sqlite3
+import pandas as pd
+import paho.mqtt.client as mqtt 
+import numpy as np
+
+## data source 
+url_tot = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv'
+url_latest = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv'
+url_region = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv'
+
+# constants
+drop_stats = ['stato','casi_da_sospetto_diagnostico', 'casi_da_screening', 
+                'note', 'note_test', 'note_casi', 'codice_nuts_1', 'codice_nuts_2']
+drop_age = ['zone','sigla','cn1', 'cn2', 'region_name', 'lat', 'long']
+col_stats = ['date', 'region_id', 'region_name', 'lat', 'long', 'recovered', 'intensive_care', 
+            'hospitalized', 'domestic_isolation', 'total_positive', 'variation_total_positive', 
+            'new_positive', 'dimessi_guariti', 'deaths', 'total_cases', 'test', 'tested_cases',
+               'new_intensive_care', 'tot_positive_pcr', 'tot_positive_antigenic', 'test_pcr', 'test_antigenic']
+
+col_region = ['region_id', 'cn1', 'zone', 'cn2','region_name', 'sigla', 'lat', 'long', 'age_group', 'population_males', 'population_females', 'population']
+
+
+def send_table(table, cols, topic):
+    client.publish("covid_italy_col", str(cols)) 
+    client.loop()
+    for index, row in table.iterrows():
+        client.publish(topic, str(list(row))) 
+        client.loop()
+
+
+#%% data with cases  and other stats
+df_stats = pd.read_csv(url_tot)                                 # load data
+df_stats = df_stats.drop(columns=drop_stats)                    # drop useless columns
+df_stats = df_stats.set_axis(col_stats, axis=1, inplace=False) # change column names
+df_stats = df_stats.replace({np.nan: None})
+
+# %% region and age data
+df_region_age = pd.read_csv(url_region)                                                                     # load data 
+df_region_age = df_region_age.set_axis(col_region, axis=1, inplace=False)                                   # change column names
+df_region = df_region_age.groupby(['region_id', 'zone', 'region_name', 'lat', 'long']).sum().reset_index()  # group by region 
+df_age = df_region_age.drop(columns=['zone','sigla','cn1', 'cn2', 'region_name', 'lat', 'long'])            # drop redundant columns
+
+
+#%% set up mqtt client
+broker_address="broker.hivemq.com"
+client = mqtt.Client("carlos")      #create new instance
+client.connect(broker_address)      #connect to broker
+
+# %% nome of the tables and the columns to store in the database 
+stats_col = {'table':'cases', 'cols': list(df_stats)}
+regions_col = {'table':'regions', 'cols': list(df_region)}
+age_col = {'table':'age', 'cols': list(df_age)}
+
+# send tables
+send_table(df_age, age_col, "covid_italy_age")                  # age table
+send_table(df_region, regions_col, "covid_italy_region")        # region table
+send_table(df_stats, stats_col, "covid_italy")                  # stats table
+
+# %%
